d9ab6b712fd165f1ab789204dfac62f9cba41c99
cubik707 <stdemidovez@gmail.com>
2026-05-17 16:05:43 +0300

    feat: configure HttpModule with timeout and keepAlive settings

--- src/presentation/recommendations/recommendations.module.ts
+++ src/presentation/recommendations/recommendations.module.ts
@@ -3,9 +3,16 @@ import { HttpModule } from '@nestjs/axios';
 import { DatabaseModule } from '../../infrastructure/database/database.module';
 import { RecommendationsController } from './recommendations.controller';
 import { GetRecommendationsUseCase } from '../../application/recommendation/use-cases/get-recommendations.use-case';
+import * as http from 'http';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [
+    DatabaseModule,
+    HttpModule.register({
+      timeout: 5000,
+      httpAgent: new http.Agent({ keepAlive: false }),
+    }),
+  ],
   controllers: [RecommendationsController],
   providers: [GetRecommendationsUseCase],
 })
